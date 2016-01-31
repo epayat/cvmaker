8576ac749b4f359d82b3ad7f50a65cd3bf5b21c2
epayat <epayat@gmail.com>
2016-01-31 15:30:16 +0100

    added some pictures

---
+++ src/main/java/com/progsan/atlantis/wicket/view/BaseWebPage.java
@@ -0,0 +1,10 @@
+package com.progsan.atlantis.wicket.view;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.markup.html.WebPage;
+
+/**
+ * Created by Erdal on 31.01.2016.
+ */
+public class BaseWebPage extends WebPage {
+}
--- src/main/java/com/progsan/atlantis/wicket/view/HomePage.java
+++ src/main/java/com/progsan/atlantis/wicket/view/HomePage.java
@@ -1,12 +1,11 @@
 package com.progsan.atlantis.wicket.view;
 
-import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 
 /**
  * Created by Erdal on 31.01.2016.
  */
-public class HomePage extends WebPage {
+public class HomePage extends BaseWebPage {
     @Override
     protected void onInitialize(){
         super.onInitialize();
